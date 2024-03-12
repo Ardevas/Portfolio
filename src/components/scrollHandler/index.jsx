e29ac574b8d7f30fc132af2b3ce26458e0462948
tho.bardot@gmail.com <tho.bardot@gmail.com>
2024-03-12 14:41:44 +0100

    design adjustments

--- src/components/scrollHandler/index.jsx
+++ src/components/scrollHandler/index.jsx
@@ -6,10 +6,10 @@ function updateActiveNavLink() {
   const sections = document.querySelectorAll("section");
 
   sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const sectionTop = section.offsetTop; // The distance from the top of the page to the top of the section
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop; // The distance the page has been scrolled from the top
 
-    if (sectionTop <= scrollTop) {
+    if (sectionTop <= scrollTop + 1) {
       const sectionId = section.getAttribute("id");
       const navLink = document.querySelector(
         `nav .nav-links a[href="#${sectionId}"]`
--- src/layout/sections/about/index.jsx
+++ src/layout/sections/about/index.jsx
@@ -9,19 +9,19 @@ const About = () => {
     <div className="about">
       <SectionTitle title="Présentation" />
       <SectionRow
-        texte="
-          Je suis un développeur web junior, passionné par la programmation et
-          le développement web. J'ai commencé à apprendre le développement web
-          il y a un an, et j'ai acquis des compétences en
-          HTML, CSS, JavaScript et React.
-
-          J'ai également acquis des compétences en développement web en suivant
-          des tutoriels sur YouTube et en suivant des cours sur la plateforme en
-          ligne OpenClassrooms.
-
-          J'ai également acquis des compétences en développement web en suivant
-          des tutoriels sur YouTube et en suivant des cours sur la plateforme en
-          ligneOpenClassrooms."
+        texte={
+          <>
+            Je suis un développeur web junior, passionné par la programmation et
+            le développement web. J'ai commencé à apprendre le développement web
+            il y a un an, et j'ai acquis des compétences en <br />
+            HTML, CSS, JavaScript et React.
+            <br />
+            <br />
+            J'ai également acquis des compétences en développement web en
+            suivant des tutoriels sur YouTube et en suivant des cours sur la
+            plateforme en ligne OpenClassrooms.
+          </>
+        }
       />
     </div>
   );
